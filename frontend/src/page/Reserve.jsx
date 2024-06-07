43eea51fc5190d451b0ef9d33aa7c2be377e1e08
KwonSanta <KwonSanta@gmail.com>
2024-06-07 13:05:09 +0900

    react: Material UI's Date and Time Picker 설치

--- frontend/src/page/Reserve.jsx
+++ frontend/src/page/Reserve.jsx
@@ -1,7 +1,7 @@
 import "react-datepicker/dist/react-datepicker.css";
 import { useState } from "react";
 import DatePicker from "react-datepicker";
-import { Box, Flex } from "@chakra-ui/react";
+import { Box } from "@chakra-ui/react";
 
 export function Reserve() {
   const [startDate, setStartDate] = useState(new Date());
@@ -12,20 +12,7 @@ export function Reserve() {
   const maxDate = new Date(today.setMonth(today.getMonth() + 3));
 
   return (
-    <Flex>
-      <Box>
-        <DatePicker
-          showIcon
-          selected={startDate}
-          onChange={(date) => setStartDate(date)}
-          dateFormat="Pp"
-          timeintervals={30} // 30분 단위로 선택 가능한 box가 나오게 설정
-          toggleCalendarOnIconClick
-          isClearable
-          showTimeSelect
-          timeIntervals={60}
-        />
-      </Box>
+    <Box>
       <Box>
         <DatePicker
           showIcon
@@ -42,6 +29,7 @@ export function Reserve() {
           maxDate={maxDate}
         />
       </Box>
-    </Flex>
+      <Box></Box>
+    </Box>
   );
 }
