d39a08e5d847cc8888e1cf9e9c37a5e39a262764
KwonSanta <KwonSanta@gmail.com>
2024-06-07 12:05:38 +0900

    react: react-datePicker 설정

--- frontend/src/page/Reserve.jsx
+++ frontend/src/page/Reserve.jsx
@@ -1,20 +1,49 @@
 import "react-datepicker/dist/react-datepicker.css";
 import { useState } from "react";
 import DatePicker from "react-datepicker";
+import { Box, Flex } from "@chakra-ui/react";
 
 export function Reserve() {
   const [startDate, setStartDate] = useState(new Date());
+  const [endDate, setEndDate] = useState(new Date());
+
+  // 날짜 객체
+  const today = new Date();
+  const maxDate = new Date(today.setMonth(today.getMonth() + 3));
 
   return (
-    <DatePicker
-      showIcon
-      selected={startDate}
-      onChange={(date) => setStartDate(date)}
-      dateFormat="Pp"
-      timeintervals={30} // 30분 단위로 선택 가능한 box가 나오게 설정
-      dateFormat="MMMM d, yyyy hh:mm aa" // June 6, 2024 10:45 PM
-      // onSelect={handleDateSelect}
-      // onChange={handleDateChange}
-    />
+    <Flex>
+      <Box>
+        <DatePicker
+          showIcon
+          selected={startDate}
+          onChange={(date) => setStartDate(date)}
+          dateFormat="Pp"
+          timeintervals={30} // 30분 단위로 선택 가능한 box가 나오게 설정
+          dateFormat="MMMM d, yyyy hh:mm aa" // June 6, 2024 10:45 PM
+          toggleCalendarOnIconClick
+          isClearable
+          showTimeSelect
+          timeIntervals={60}
+        />
+      </Box>
+      <Box>
+        <DatePicker
+          showIcon
+          selected={endDate}
+          onChange={(date) => setEndDate(date)}
+          dateFormat="Pp"
+          timeintervals={30} // 30분 단위로 선택 가능한 box가 나오게 설정
+          dateFormat="MMMM d, yyyy hh:mm aa" // June 6, 2024 10:45 PM
+          toggleCalendarOnIconClick
+          isClearable
+          showTimeSelect
+          timeIntervals={60}
+          inline
+          minDate={new Date()}
+          maxDate={maxDate}
+        />
+      </Box>
+    </Flex>
   );
 }
